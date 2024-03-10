acb39de693f62dce4390826b5465ca547d9ec3b0
laura <laura.bissoltan@nu.edu.kz>
2024-03-10 12:11:41 +0500

    find by first name was changed to the list

--- src/main/java/kz/laurabissoltan/studentpracticeproject/dao/StudentRepository.java
+++ src/main/java/kz/laurabissoltan/studentpracticeproject/dao/StudentRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
-    Student findStudentByFirstName(String firstName);
+    List<Student> findStudentByFirstName(String firstName);
     List<Student> getStudentsByAge(int age);
 }
