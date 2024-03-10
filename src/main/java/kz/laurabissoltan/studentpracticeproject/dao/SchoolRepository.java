ac4075949f69b038e51382aec283a7635d033d95
laura <laura.bissoltan@nu.edu.kz>
2024-03-10 12:01:55 +0500

    added annotations

--- src/main/java/kz/laurabissoltan/studentpracticeproject/dao/SchoolRepository.java
+++ src/main/java/kz/laurabissoltan/studentpracticeproject/dao/SchoolRepository.java
@@ -3,9 +3,11 @@ package kz.laurabissoltan.studentpracticeproject.dao;
 import kz.laurabissoltan.studentpracticeproject.model.School;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface SchoolRepository extends JpaRepository<School, Long> {
     List<School> findSchoolByName(String schoolName);
 
--- src/main/java/kz/laurabissoltan/studentpracticeproject/dao/StudentRepository.java
+++ src/main/java/kz/laurabissoltan/studentpracticeproject/dao/StudentRepository.java
@@ -3,9 +3,11 @@ package kz.laurabissoltan.studentpracticeproject.dao;
 import kz.laurabissoltan.studentpracticeproject.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
     Student findStudentByFirstName(String firstName);
     List<Student> getStudentsByAge(int age);
